83084170bc45442cbb9a6bfcfed2f1ee81ae68ae
Bruno Lottero <brunoelottero@gmail.com>
2023-05-13 14:29:10 -0300

    Implemented sphere intersection

--- lib/primitives/sphere.ex
+++ lib/primitives/sphere.ex
@@ -1,16 +1,57 @@
 defmodule Primitives.Sphere do
-  defstruct center: Graphmath.Vec3.create(), radius: 0.0, transform: Graphmath.Mat44.identity()
+  defstruct center: Graphmath.Vec3.create(), radius: 0.0, transform: Graphmath.Mat44.identity(), inverse_transform: Graphmath.Mat44.inverse(Graphmath.Mat44.identity())
 
-  @spec from({any, any, any}, any, [[number]]) :: %Primitives.Sphere{
-          center: {any, any, any},
-          radius: any,
-          transform: [[number]]
-        }
   def from(center, radius, transform) do
-    %Primitives.Sphere{center: center, radius: radius, transform: transform}
+    %Primitives.Sphere{
+      center: center,
+      radius: radius,
+      transform: transform,
+      inverse_transform: Graphmath.Mat44.inverse(transform)
+    }
   end
 
   def from() do
     %Primitives.Sphere{}
+  end
+
+  def intersect(sphere, ray, transform) do
+    { direction_x, direction_y, direction_z } = ray.direction
+    { origin_x, origin_y, origin_z } = ray.eye
+
+    { direction_x, direction_y, direction_z, _ } = Graphmath.Mat44.apply_left({direction_x, direction_y, direction_z, 0.0}, sphere.inverse_transform)
+    ray_direction = Graphmath.Vec3.normalize(Graphmath.Vec3.create(direction_x, direction_y, direction_z))
+
+    { origin_x, origin_y, origin_z, _ } = Graphmath.Mat44.apply_left({origin_x, origin_y, origin_z, 1.0}, sphere.inverse_transform)
+    ray_origin = Graphmath.Vec3.normalize(Graphmath.Vec3.create(origin_x, origin_y, origin_z))
+
+    ray_origin_center = Graphmath.Vec3.subtract(ray_origin, sphere.center)
+
+    a = Graphmath.Vec3.dot(ray_direction, ray_direction)
+    b = 2*(Graphmath.Vec3.dot(ray_direction, ray_origin_center))
+    c = Graphmath.Vec3.dot(ray_origin_center, ray_origin_center) - sphere.radius*sphere.radius
+    discriminant = b*b - 4*a*c
+
+    cond do
+      discriminant >= 0.0 -> compute_roots(a, b, discriminant, ray_origin, ray_direction, ray.eye, transform)
+      true -> 0.0
+    end
+  end
+
+  defp compute_roots(a, b, discriminant, ray_origin, ray_direction, ray_eye, transform) do
+    first_root = (-b + :math.sqrt(discriminant))/2*a
+    second_root = (-b - :math.sqrt(discriminant))/2*a
+
+    intersection = cond do
+      first_root > 0.0 and second_root > 0.0 and first_root > second_root -> second_root
+      first_root > 0.0 and second_root > 0.0 and first_root < second_root -> first_root
+      first_root < 0.0 and second_root > 0.0 -> second_root
+      first_root > 0.0 and second_root < 0.0 -> first_root
+      true -> 0.0
+    end
+
+    { point_x, point_y, point_z } = Graphmath.Vec3.add(ray_origin, Graphmath.Vec3.scale(ray_direction, intersection))
+    { point_x, point_y, point_z, _ } = Graphmath.Mat44.apply_left({point_x, point_y, point_z, 1.0}, transform)
+    intersection = Graphmath.Vec3.subtract(Graphmath.Vec3.create(point_x, point_y, point_z), ray_eye)
+    Graphmath.Vec3.length(intersection)
   end
 end
--- test/primitives/sphere_test.exs
+++ test/primitives/sphere_test.exs
@@ -4,12 +4,12 @@ defmodule JartTest.Primitives.Sphere do
 
   test "a sphere can be generated as a point in origin by default" do
     sphere = Primitives.Sphere.from()
-    assert sphere == %Primitives.Sphere{center: Graphmath.Vec3.create(), radius: 0.0}
-  end
-
-  test "a sphere has indentity transform matrix by default" do
-    sphere = Primitives.Sphere.from()
-    assert sphere == %Primitives.Sphere{transform: Graphmath.Mat44.identity()}
+    assert sphere == %Primitives.Sphere{
+      center: Graphmath.Vec3.create(),
+      radius: 0.0,
+      transform: Graphmath.Mat44.identity(),
+      inverse_transform: Graphmath.Mat44.inverse(Graphmath.Mat44.identity())
+    }
   end
 
   test "a sphere can be generated with a given center and radius" do
@@ -18,7 +18,22 @@ defmodule JartTest.Primitives.Sphere do
     assert sphere == %Primitives.Sphere{
       center: Graphmath.Vec3.create(5, 8, -122),
       radius: 677.22,
-      transform: transform
+      transform: Graphmath.Mat44.identity(),
+      inverse_transform: Graphmath.Mat44.inverse(Graphmath.Mat44.identity())
     }
+  end
+
+  test "a sphere does not intersects with a ray" do
+    transform = Graphmath.Mat44.identity()
+    ray = Primitives.Ray.from(Graphmath.Vec3.create(14, 15, 0), Graphmath.Vec3.create(0, 0, 1))
+    sphere = Primitives.Sphere.from(Graphmath.Vec3.create(0, 0, 0), 14, transform)
+    assert Primitives.Sphere.intersect(sphere, ray, transform) > 0.0
+  end
+
+  test "a sphere intersects with a ray" do
+    transform = Graphmath.Mat44.identity()
+    ray = Primitives.Ray.from(Graphmath.Vec3.create(1, 1, 0), Graphmath.Vec3.create(0, 0, 1))
+    sphere = Primitives.Sphere.from(Graphmath.Vec3.create(0, 0, 0), 14, transform)
+    assert Primitives.Sphere.intersect(sphere, ray, transform) > 0.0
   end
 end
